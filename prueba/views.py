4c8706f6d8587a26b9285c8763965384f393a7a0
Marcelo <mcacace@gmail.com>
2020-06-03 12:35:48 -0300

    mnpkkkkkkkshkd

--- prueba/views.py
+++ prueba/views.py
@@ -18,7 +18,7 @@ def saludo(request):
     temas_del_curso = ["Plantillas","Modelos","Formularios","Vistas","Despliegue"]
     #temas_del_curso = []
 
-    p1 = Persona("Marcelo", "KKC")
+    p1 = Persona("Marcelo", "Cacace")
 
     fecha_actual = datetime.datetime.now()
 
